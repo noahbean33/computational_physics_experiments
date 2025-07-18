b8fcd17fcc15ca00e36a9e6343f593c41cd8f7b7
Noah Bean <92399044+noahbean33@users.noreply.github.com>
2025-07-17 23:44:57 -0700

    added content from the computational physics and numerical methods classes

---
+++ NumPyOperations.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[1, 2], [2, 1]])
+
+c = np.matmul(a, b)
+
+# inner product (vector-vector multiplication)
+v1 = np.array([1, 2, 3])
+v2 = np.array([2, 3, 4])
+
+print(np.dot(v1, v2))
